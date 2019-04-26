043c99bb51753329183ea3fae435f089215f65dd
abhihanda23 <abhihanda23@gmail.com>
2019-04-27 00:38:52 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,42 @@
+import time
+from flask import Flask, jsonify
+from multiprocessing import Process, Value
+
+
+app = Flask(__name__)
+
+
+tasks = [
+   {
+      'id': 1,
+      'title': u'Buy groceries',
+      'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+      'done': False
+   },
+   {
+      'id': 2,
+      'title': u'Learn Python',
+      'description': u'Need to find a good Python tutorial on the web', 
+      'done': False
+   }
+]
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+   return jsonify({'tasks': tasks})
+
+
+def record_loop(loop_on):
+   while True:
+      if loop_on.value == True:
+         print("loop running")
+      time.sleep(1)
+
+
+if __name__ == "__main__":
+   recording_on = Value('b', True)
+   p = Process(target=record_loop, args=(recording_on,))
+   p.start()  
+   app.run(debug=True, use_reloader=False)
+   p.join()
